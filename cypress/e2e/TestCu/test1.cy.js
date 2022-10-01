43c465799d2d89dcdabbd3683e65126d9ade96be
mrozwat <ozwatandreev@gmail.com>
2022-10-02 00:36:41 +0300

    first test ready

--- cypress/e2e/TestCu/test1.cy.js
+++ cypress/e2e/TestCu/test1.cy.js
@@ -4,9 +4,14 @@ describe('login',function(){
 
         cy.contains("Войти").click()
 
-        cy.get('input[type="tel"]').type('9373970242')
+        cy.get('input[type="tel"]').type('9373970242').should('have.value','+7 (937) 397-02-42')
 
         cy.contains("Продолжить").click()
-        
+
+        cy.get('.login-form__header_indent_sm').should('have.text',' Подтверждение ')
+
+        cy.get('.pin-form-phone').should('have.text',' +7 (937) 397-02-42 ')
     })
 })
+
+
