525dbe7602c3973a3f7f0361eee9c937d6bfb489
Sergey Andreev <ozwatandree@gmail.com>
2022-10-14 23:02:29 +0300

    fast fix

--- cypress/e2e/TestCu/grifner.cy.js
+++ cypress/e2e/TestCu/grifner.cy.js
@@ -16,7 +16,7 @@ it('grifner input',()=>{
     cy.xpath('/html/body/div[2]/main/div[1]/div[2]/div[4]/div/div/div/input').type('qa').should('have.value','qa').type('{enter}');
     // cy.get('.sc-tghwm-0').
     cy.xpath('/html/body/div[2]/main/div[1]/div/div/div[1]/div/div/input').should('have.value','qa');
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get(':nth-child(1) > .sc-g7i5pe-1 > .sc-1m292s3-1 > .sc-1m292s3-0').click();
     cy.get('.sc-1xj3xob-0').click();
     cy.get('#page-root > :nth-child(2) > :nth-child(2)').should('be.visible');
